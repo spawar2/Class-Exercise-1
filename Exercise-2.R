b986e5720232b8fe7da232d9f2a1f39e87c7da4d
spawar2 <spawar2@student.gsu.edu>
2018-08-27 12:20:59 -0400

    commit 2

--- Exercise-2.R
+++ Exercise-2.R
@@ -20,6 +20,78 @@ lines(density2, col = "red")
 density3 <- density(mas5.e[, 3])
 lines(density3, col = "blue")
 
+# Making MA plots 
+# M: difference in average log intensities
+# A: average log intensities
+
+d <- rowMeans(mas5.e[,1:3]) - rowMeans(mas5.e[,4:6])
+a <- rowMeans(mas5.e)
+plot(a, d, ylim = c(-5, 5), main = "MAS 5.0 MA plot", xlab = "A", ylab = "M", pch = ".")
+abline(h = c(-1, 1))
+
+#Finding specific probesets in the plot
+
+spikedin <- colnames(pData(spikein95))
+spikedIndex <- match(spikedin,featureNames(mas5.eset))
+points(a[spikedIndex], d[spikedIndex],pch = 19, col = "red")
+
+# Trying RMA normalization
+
+rma.eset <- rma(spikein95)
+rma.e <- exprs(rma.eset)
+
+# Making MA plots 
+
+d <- rowMeans(rma.e[,1:3]) - rowMeans(rma.e[,4:6])
+a <- rowMeans(rma.e)
+plot(a, d, ylim = c(-2, 2), main = "RMA MA plot", xlab = "A", ylab = "M", pch = ".")
+abline(h = c(-1, 1))
+
+#Finding specific probesets in the plot
+
+points(a[spikedIndex], d[spikedIndex],pch = 19, col = "red")
+
+# Volcano Plots
+source("https://bioconductor.org/biocLite.R")
+biocLite("genefilter")
+library("genefilter")
+pData(rma.eset) <- pData(mas5.eset)
+tt <- rowMeans(rma.e)
+lod <- tt
+plot(d, lod, cex = 0.25, main = "Volcano plot for MA", xlim = c(-2, 2), xlab = "M", ylab = "A", yaxt = "n")
+axis(2, at = seq(0, 3, by = 1), labels = 10^(-seq(0, 3, by = 1)))
+points(d[spikedIndex], lod[spikedIndex], pch = 19, col = "red")
+abline(h = 2, v = c(-1, 1))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
