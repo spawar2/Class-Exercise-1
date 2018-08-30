1bb4ce9b37f1218ddaf9da5512cb8228993994ea
spawar2 <spawar2@student.gsu.edu>
2018-08-29 20:48:14 -0400

    Final_Microarray

--- Exercise-2.R
+++ Exercise-2.R
@@ -64,6 +64,138 @@ points(d[spikedIndex], lod[spikedIndex], pch = 19, col = "red")
 abline(h = 2, v = c(-1, 1))
 
 
+# Exercise to practice
+
+https://rawgit.com/bioinformatics-core-shared-training/microarray-analysis/master/de-tutorial.html
+
+https://rawgit.com/bioinformatics-core-shared-training/microarray-analysis/master/affymetrix.nb.html
+
+library(affy)
+setwd("C:/Users/Bio-user/Documents/GitHub/Class-Exercise-1/estrogen")
+targetsFile <- "estrogen.txt"
+pd <- read.AnnotatedDataFrame(targetsFile,header=TRUE,sep="",row.names=1)
+pData(pd)
+
+
+ER <- pData(pd)$estrogen
+Time <- factor(pData(pd)$time.h)
+design <- model.matrix(~ER+Time)
+design
+
+design2 <- model.matrix(~ER*Time)
+design2
+
+raw <-ReadAffy(celfile.path = "C:/Users/Bio-user/Documents/GitHub/Class-Exercise-1/estrogen", filenames=rownames(pData(pd)),phenoData = pd)
+raw
+
+boxplot(raw,col="red",las=2)
+
+par(mfrow=c(2,1))
+hist(log2(pm(raw[,1])),breaks=100,col="steelblue",main="PM",xlim=c(4,14))
+hist(log2(mm(raw[,1])),breaks=100,col="steelblue",main="MM",xlim=c(4,14))
+
+
+source("https://bioconductor.org/biocLite.R")
+biocLite("affyPLM")
+
+library(affyPLM)
+
+plmset <- fitPLM(raw)
+NUSE(plmset,las=2)
+
+bad <- ReadAffy(celfile.path = "C:/Users/Bio-user/Documents/GitHub/Class-Exercise-1/estrogen",filenames="bad.cel")
+image(bad)
+
+par(mfrow=c(2,4))
+image(raw[,1])
+
+eset <- rma(raw)
+
+library(limma)
+fit1 <- lmFit(eset, design)
+fit1 <- eBayes(fit1)
+topTable(fit1, coef=2)
+
+
+fit2 <- lmFit(eset, design2)
+fit2 <- eBayes(fit2)
+topTable(fit2, coef=2)
+
+#Annotation of samples
+
+library(GEOquery)
+library(limma)
+url <- "ftp://ftp.ncbi.nlm.nih.gov/geo/series/GSE33nnn/GSE33126/matrix/GSE33126_series_matrix.txt.gz"
+filenm <- "GSE33126_series_matrix.txt.gz"
+if(!file.exists(filenm)) download.file(url, destfile=filenm)
+gse <- getGEO(filename=filenm)
+
+gse
+head(exprs(gse))
+
+exprs(gse) <- log2(exprs(gse))
+boxplot(exprs(gse),outline=FALSE)
+
+pd <- pData(gse)
+SampleGroup <- pd$source_name_ch1
+library(genefilter)
+destats <- rowttests(exprs(gse),fac=SampleGroup)
+head(destats)
+
+library(genefilter)
+gse.expFilt <- varFilter(gse)
+gse.expFilt
+
+# Annotation
+
+anno <- fData(gse.expFilt)
+head(anno)[,1:5]
+
+anno <- anno[,c("Symbol","Entrez_Gene_ID","Chromosome","Cytoband")]
+fit2$genes <- anno
+topTable(fit2)
+
+volcanoplot(fit2)
+
+testResults <- topTable(fit2, number=nrow(fit2))
+testResults[which(testResults$Symbol == "OCIAD2"),]
+
+mylist <- c("LOC441066","ARF3","FMNL3","CSF1R","XLKD1","TTRAP","DMWD","SYNPO2L","PILRB","LAMP3")
+testResults[which(testResults$Symbol %in% mylist),]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
